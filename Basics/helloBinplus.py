d5e83ae681c91f30f9f768ce8cfebe5a30b5797c
imvivek71 <38568313+imvivek71@users.noreply.github.com>
2018-12-31 21:49:49 +0530

    Create helloBinplus.py
    It's the first program, to welcome the user after pressing any  letter

---
+++ Basics/helloBinplus.py
@@ -0,0 +1,11 @@
+# Enter any key and get a welcome output
+"""
+Created on Wed Dec 19 16:59:35 2018
+
+@author: vivek
+"""
+greetings = input("Enter any letter")
+if len(greetings) >0:
+    print("Binplus Tech, we are happy to help you")
+else:
+    print("You did not entered anything, kindly enter any letter")
