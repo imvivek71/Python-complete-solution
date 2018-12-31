2a01a07bdfede02c3b0bd53463efad1c54c8171b
imvivek71 <38568313+imvivek71@users.noreply.github.com>
2019-01-01 00:19:27 +0530

    Create pattern5th.py

---
+++ Task3PatternFun/pattern5th.py
@@ -0,0 +1,18 @@
+""""
+ 
+ * 
+
+  * * 
+
+   * * * 
+
+    * * * * 
+
+     * * * * *
+"""
+for i in range(6):
+    for z in range(i):
+        print(" ",end="")
+    for j in range(0,i):
+        print("*",end=" ")
+    print("\n")
