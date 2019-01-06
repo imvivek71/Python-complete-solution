98f70bebc6f5010c988feda433999953ae06e9e1
Vivek Goswami <38568313+imvivek71@users.noreply.github.com>
2019-01-06 20:57:39 +0530

    Create StringSpecificformat.py

---
+++ Task8/StringSpecificformat.py
@@ -0,0 +1,2 @@
+print('Twinkle, twinkle, little star,\n\t   How I wonder what you are!\n\t\t\t    Up above the world\
+ so high,\n\t\t\t    Like a diamond in the sky.\nTwinkle, twinkle, little star,\n\t   How I wonder what you are.')
